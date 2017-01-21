7febcaf128077c2861790cfcbf6998eed4e4a58f
rockdong <rockdong@1234567.local>
2017-01-21 21:58:19 +0800

    dssms 工程环境初始化

---
+++ README.md
@@ -0,0 +1,1 @@
+# dssms_env
---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/user/project_env/dssms_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
